075c40e124443ef56bad2a75511e8b553e6792d1
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-09-27 17:09:08 +0200

    Allow to add headers to the requests

--- actions.js
+++ actions.js
@@ -3,8 +3,11 @@ import uuid from 'react-native-uuid'
 import genActionTypes from './actionTypes'
 
 
-function actions(basePath, baseUrl='')
+function actions(basePath, options={})
 {
+  const headers = options.headers
+
+  let baseUrl = options.baseUrl || ''
   if(baseUrl.endsWith('/')) baseUrl = baseUrl.slice(0, baseUrl.length-1)
 
   const baseType = basePath.slice(0, basePath.length-1).toUpperCase()
@@ -23,7 +26,13 @@ function actions(basePath, baseUrl='')
           id,
           offline:
           {
-            effect: {url: `${baseUrl}/${basePath}`, method: 'POST', body},
+            effect:
+            {
+              url: `${baseUrl}/${basePath}`,
+              method: 'POST',
+              body,
+              headers
+            },
             commit: {type: actionTypes.add_commit, meta: {id}},
             rollback: {type: actionTypes.add_rollback, meta: {id}}
           }
@@ -41,7 +50,13 @@ function actions(basePath, baseUrl='')
           id,
           offline:
           {
-            effect: {url: `${baseUrl}/${basePath}/${id}`, method: 'PUT', body},
+            effect:
+            {
+              url: `${baseUrl}/${basePath}/${id}`,
+              method: 'PUT',
+              body,
+              headers
+            },
             commit: {type: actionTypes.update_commit, meta: {id}},
             rollback: {type: actionTypes.update_rollback, meta: {id}}
           }
@@ -58,7 +73,12 @@ function actions(basePath, baseUrl='')
           id,
           offline:
           {
-            effect: {url: `${baseUrl}/${basePath}/${id}`, method: 'DELETE'},
+            effect:
+            {
+              url: `${baseUrl}/${basePath}/${id}`,
+              method: 'DELETE',
+              headers
+            },
             commit: {type: actionTypes.delete_commit, meta: {id}},
             rollback: {type: actionTypes.delete_rollback, meta: {id}}
           }
